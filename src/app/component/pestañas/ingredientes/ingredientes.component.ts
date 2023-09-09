f1f1916395da47d00a3c35ee9252bd2aaa7dad9a
x1n4px <silosabia@gmail.com>
2023-09-09 18:57:56 +0200

    Cambio de variable ingredientes del service ingredientes por un json que recoje los datos en formato json, modificado el service completo para que lea el fichero json y devuelva los datos de forma asincrona

--- src/app/component/pestañas/ingredientes/ingredientes.component.ts
+++ src/app/component/pestañas/ingredientes/ingredientes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IngredientesService } from 'src/app/service/ingredientes.service';
 import { IngredientesModalComponent } from '../ingredientes-modal/ingredientes-modal.component';
 import { MatDialog } from '@angular/material/dialog';
@@ -8,15 +8,25 @@ import { MatDialog } from '@angular/material/dialog';
   templateUrl: './ingredientes.component.html',
   styleUrls: ['./ingredientes.component.css']
 })
-export class IngredientesComponent {
+export class IngredientesComponent implements OnInit{
   listaIngredientes:any[] = [];
   listaIngredientesFiltrados:any[] = [];
   btnF:any = 'TODOS';
 
 
   constructor(private ingredientesService: IngredientesService, private dialog: MatDialog){
-    this.listaIngredientes = ingredientesService.devolverTodo();
     this.filtrar();
+  }
+
+  ngOnInit(): void {
+    this.obtenerDatos();
+  }
+
+  obtenerDatos() {
+    this.ingredientesService.devolverTodo().subscribe(data => {
+      this.listaIngredientes = data;
+      this.listaIngredientesFiltrados = data;
+     });
   }
 
   btnSelector(selector: any) {
@@ -28,7 +38,7 @@ export class IngredientesComponent {
     if(this.btnF === 'TODOS'){
       this.listaIngredientesFiltrados = this.listaIngredientes;
     }else{
-        this.listaIngredientesFiltrados = this.listaIngredientes.filter(item => item.tags === this.btnF);
+         this.listaIngredientesFiltrados = this.listaIngredientes.filter(item => item.tags === this.btnF);
     }
   }
 
